7dc237fabcfcabcb0b0a0ea9af4b2b2d78215bed
ahmed3bdo008 <ahmed3bdo008@gmail.com>
2022-08-21 13:55:44 -0700

    Add  authencation

--- Back/EducationMvc/Extension/ConfigurationAuthenticationScheme.cs
+++ Back/EducationMvc/Extension/ConfigurationAuthenticationScheme.cs
@@ -1,18 +1,34 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EducationMvc.Extension
 {
     public static class ConfigurationAuthenticationScheme
     {
        
-        public static void AddAuthenticationConfig(this IServiceCollection services)
+        public static void AddAuthenticationConfig(this IServiceCollection services,IConfiguration configuration )
         {
             services.AddAuthentication(Option=>{
                 Option.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
                 Option.DefaultScheme=JwtBearerDefaults.AuthenticationScheme;
                 Option.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
-                }).AddJwtBearer();
+                }).AddJwtBearer(
+                  Jwt=>{
+                    var  key=Encoding.ASCII.GetBytes(configuration["jwt.Key"]);
+                    Jwt.RequireHttpsMetadata=false;
+                    Jwt.SaveToken=false;
+                    Jwt.TokenValidationParameters=new Microsoft.IdentityModel.Tokens.TokenValidationParameters{
+                        ValidateAudience=true,
+                        ValidAudience=configuration["jwt.aud"],
+                        ValidateIssuer=true,
+                        ValidIssuer=configuration["jwt.iss"],
+                        IssuerSigningKey=new SymmetricSecurityKey(key)
+                    };
+                  }
+                );
         }
     }
 }
--- Back/EducationMvc/Startup.cs
+++ Back/EducationMvc/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EducationMvc.Extension;
 using EducationMvc.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,6 +36,7 @@ namespace EducationMvc
             //Authenication 
             services.AddAuthentication();
             services.ConfigurationIdentity();
+            services.AddAuthenticationConfig(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
